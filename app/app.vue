2a36dc52ef2abca4ae4b87752a5021f3f296e0ce
Torsten Schmidt <torsten.max.schmidt@gmail.com>
2025-10-21 16:47:01 +1000

    feat: update favicon assets and theme color, add web app manifest and SVG icon

--- app/app.vue
+++ app/app.vue
@@ -7,13 +7,13 @@
 useHead({
   link: [
     { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
     { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
     { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
   ],
   meta: [
-    { name: 'theme-color', content: '#8B4513' }, // Coffee brown color
-    { name: 'msapplication-TileColor', content: '#8B4513' }
+    { name: 'theme-color', content: '#0ea5e9' },
+    { name: 'msapplication-TileColor', content: '#0ea5e9' }
   ]
 })
 
